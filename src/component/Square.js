1ab0dece5dd8ae8fa0e4a273f87e4a5d66fecdee
Geidy <geidydm@gmail.com>
2019-07-21 18:29:18 -0400

    created a component file

---
+++ src/component/Square.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Square extends React.Component{
+    render(){
+        return (
+            //onClick arrow function anonymous function
+            <button className="square" >  
+                
+
+            </button>
+        );
+    }
+}
+
+
+export default Square;
