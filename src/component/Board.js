47bc0ebd8a4f78f7acb61edff2b0c94c1497b2f3
Geidy <geidydm@gmail.com>
2019-07-22 01:41:06 -0400

    added handleClick function so the Board ahs complete control of the square component

--- src/component/Board.js
+++ src/component/Board.js
@@ -7,6 +7,13 @@ class Board extends React.Component{
         this.state = {squares: Array(9).fill(null),
         };
     }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+      }
+      
     renderSquare(i) {
         return (<Square value={this.state.squares[i]}
         onClick={() => this.handleClick(i)} />
