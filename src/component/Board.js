ee2a34fb6a3edbabfc95774e8412c7912c12da19
Geidy <geidydm@gmail.com>
2019-07-24 15:59:52 -0400

    Added xIsNext boolean variable to determine which player goes next

--- src/component/Board.js
+++ src/component/Board.js
@@ -5,13 +5,16 @@ class Board extends React.Component{
     constructor(props){
         super(props);
         this.state = {squares: Array(9).fill(null),
+            xIsNext: true,  //It will flips to Determines which player goes next.
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X': 'O';
+        this.setState({squares: squares,
+        xIsNext: !this.state.xIsNext
+    });
       }
       
     renderSquare(i) {
