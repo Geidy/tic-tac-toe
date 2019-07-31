cf7aef0f25237b47e08c04ced8c2861744363df2
Geidy <geidydm@gmail.com>
2019-07-31 18:02:54 -0400

    Added and created navbar and commented it

--- src/component/Game.js
+++ src/component/Game.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Styles from '../css/Styles.css';
 import Board from './Board';
+//import Navbar from '../component/Navbar';
 
 class Game extends React.Component {
     render() {
       return (
         <div className="game">
           <div className="game-board">
+            
             <Board />
           </div>
           <div className="game-info">
---
+++ src/component/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar(){
+    return(
+        <div>
+            <Navbar>
+                Welcome to Tic-Tac-Toe
+            </Navbar>
+        </div>
+    )
+}
+
+export default Navbar;
