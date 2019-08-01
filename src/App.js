7dd5bc4c4007b7e7b4d60a25b267e5e8d84b74fd
Geidy <geidydm@gmail.com>
2019-08-01 04:01:09 -0400

    Added the new Navbar path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Game from './component/Game';
+import Navbar from './component/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Game />
     </div>
   );
