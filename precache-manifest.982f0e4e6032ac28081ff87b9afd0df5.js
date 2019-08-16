493a4a6a58e01769f08ca7eb86a0f0c7025498ad
Geidy <geidydm@gmail.com>
2019-08-16 13:06:36 -0400

    Updates

---
+++ precache-manifest.982f0e4e6032ac28081ff87b9afd0df5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b504e06a58bfeba4be76558532c2b8d5",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "54b904fda90016240eeb",
+    "url": "/tic-tac-toe/static/css/main.d3527fc4.chunk.css"
+  },
+  {
+    "revision": "6afb4d93dc025bca6d86",
+    "url": "/tic-tac-toe/static/js/2.74286c36.chunk.js"
+  },
+  {
+    "revision": "54b904fda90016240eeb",
+    "url": "/tic-tac-toe/static/js/main.2d511ba6.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
