933ada298008418f138d2ca56211971fb7d476dd
Geidy <geidydm@gmail.com>
2019-08-17 18:41:19 -0400

    Created EmojiData.js object with the emojies icons

---
+++ src/component/EmojiData.js
@@ -0,0 +1,28 @@
+
+const EmojiData = [
+
+        {id: 1, icon:"😀"},
+        {id: 2, icon:"😂"},
+        {id: 3, icon:"😎"},
+        {id: 4, icon:"😍"},
+        {id: 5, icon:"🤑"},
+        {id: 6, icon:"🤬"},
+        {id: 7, icon:"🤡"},
+        {id: 8, icon:"💀"},
+        {id: 9, icon:"👽"},
+        {id: 10, icon:"👩🏾‍🚀"},
+        {id: 11, icon:"👩🏾‍🎓"},
+        {id: 12, icon:"👩🏾‍💻"},
+        {id: 13, icon:"💃🏽"},
+        {id: 14, icon:"🕺🏽"},
+        {id: 15, icon:"🕵🏽"},
+        {id: 16, icon:"🐘"},
+        {id: 17, icon:"🦄"},
+        {id: 18, icon:"🐆"},
+        {id: 19, icon:"🌈"},
+        {id: 20, icon:"💐"},
+        {id: 21, icon:"🥑"},
+
+]
+
+export default EmojiData;
